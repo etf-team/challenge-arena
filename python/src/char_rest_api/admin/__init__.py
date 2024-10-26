f1bd3c21f238b6e439168ef1b096265feba83698
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2024-10-26 04:09:06 +0300

    Fixup admin

--- python/src/char_rest_api/admin/__init__.py
+++ python/src/char_rest_api/admin/__init__.py
@@ -1,6 +1,12 @@
+from sqladmin import Admin
+from sqlalchemy.ext.asyncio import AsyncEngine
+
 from dishka import AsyncContainer
 from fastapi import FastAPI
 
 
-async def setup_admin(container: AsyncContainer, app: FastAPI):
-    pass
+async def setup_admin(container: AsyncContainer, app: FastAPI) -> Admin:
+    engine = await container.get(AsyncEngine)
+    admin = Admin(app, engine=engine)
+
+    return admin
