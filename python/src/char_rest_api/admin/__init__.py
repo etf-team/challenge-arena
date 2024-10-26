0339bbebd1f4a7f68b46c02bcb95c82c927ea7d9
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2024-10-26 04:25:58 +0300

    Fix admin: add auth

--- python/src/char_rest_api/admin/__init__.py
+++ python/src/char_rest_api/admin/__init__.py
@@ -1,15 +1,30 @@
+from datetime import timedelta
+
 from sqladmin import Admin
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from dishka import AsyncContainer
 from fastapi import FastAPI
 
+from char_rest_api.admin.auth_backend import AdminAuthBackend
 from char_rest_api.admin.views import UserAdmin
+from char_rest_api.infrastructure import AdminConfig
 
 
 async def setup_admin(container: AsyncContainer, app: FastAPI) -> Admin:
     engine = await container.get(AsyncEngine)
-    admin = Admin(app, engine=engine)
+    config: AdminConfig = await container.get(AdminConfig)
+
+    admin = Admin(
+        app,
+        engine=engine,
+        authentication_backend=AdminAuthBackend(
+            secret_key=config.secret_key,
+            username=config.username,
+            password=config.password,
+            login_duration=timedelta(days=3),
+        )
+    )
 
     admin.add_view(UserAdmin)
 
